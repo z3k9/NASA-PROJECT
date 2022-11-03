f6bab6e9e6cd106892651ef37e7f1389562de489
z3k9 <ajelelade@gmail.com>
2022-11-03 18:38:45 +0100

    updated the planets model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -3,8 +3,6 @@ const planets = require('./planets.mongo');
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const habitablePlanets = []
-
 function isHabitablePlanet(planet){
     return planet['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
