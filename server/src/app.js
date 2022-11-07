7d973087fe29da7557e8b40400999f8079c0f4b8
z3k9 <ajelelade@gmail.com>
2022-11-07 14:24:46 +0100

    implemented versioning on api

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require(('path'));
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
 const cors = require('cors');
 const morgan = require('morgan');
+const api = require('./routes/api');
 
 // setting up our middleware
 app.use(cors({
@@ -14,10 +13,7 @@ app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
-// setting up express router
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1',api);
 
 
 app.get('/*',(req,res)=>{
