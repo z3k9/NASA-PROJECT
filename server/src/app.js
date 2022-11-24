59a12d296dad68fdafbbf6e610f4b6ca7ea851c6
z3k9 <ajelelade@gmail.com>
2022-11-24 14:37:01 +0100

    set up build yaml

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,14 @@ const path = require(('path'));
 const cors = require('cors');
 const morgan = require('morgan');
 const v1 = require('./routes/v1');
+const helmet = require('helmet');
 
 // setting up our middleware
+app.use(helmet);
 app.use(cors({
     origin:'http://localhost:3000',
 }));
 app.use(morgan("combined"));
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/v1', v1);
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
-const http = require('http');
+const https = require('https');
+
+const fs = require('fs');
 
 require('dotenv').config();
 
@@ -8,7 +10,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
+const server = https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+},app);
 
 const {loadPlanetsData} = require('./models/planets.model');
 
